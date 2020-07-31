506dc7d30fef273d6c4bf86711fa3c82bd21dede
adorigo <adorigo@student.s19.be>
2020-07-31 16:58:28 +0200

    commented the main function

--- src/main.c
+++ src/main.c
@@ -6,11 +6,17 @@
 /*   By: adorigo <adorigo@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/16 14:44:33 by adorigo           #+#    #+#             */
-/*   Updated: 2020/06/05 18:25:40 by adorigo          ###   ########.fr       */
+/*   Updated: 2020/07/31 12:02:10 by adorigo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+/*
+** the main function of the cub3d project will check how many arguments were
+** passed to the executable, and will execute the program differently according
+** to how many arguments it got
+*/
 
 int	main(int ac, char **av)
 {
